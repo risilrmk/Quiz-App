8e8395408070593d44dcf48e1799ff3de134335b
Muhammed Risil <87575552+risilrmk@users.noreply.github.com>
2024-07-24 06:39:47 +0530

    Add files via upload

---
+++ JS/questions.js
@@ -0,0 +1,47 @@
+const questions = [
+    {
+        question: 'Which is Largest Animal in The World',
+        answers: [
+            { text: 'Shark', correct: false },
+            { text: 'Blue Whale', correct: true },
+            { text: 'Elephant', correct: false },
+            { text: 'Girraffe', correct: false }
+        ]
+    },
+    {
+        question: 'Which is the Largest Continent in The Earth',
+        answers: [
+            { text: 'Antartica', correct: false },
+            { text: 'Africa', correct: false },
+            { text: 'Asia', correct: true },
+            { text: 'Australia', correct: false }
+        ]
+    },
+    {
+        question: 'Which is the Smallest Continent in The Earth',
+        answers: [
+            { text: 'Antartica', correct: false },
+            { text: 'Africa', correct: false },
+            { text: 'Asia', correct: false },
+            { text: 'Australia', correct: true }
+        ]
+    },
+    {
+        question: 'Who Acted as the Protogonist in the Film "The Matrix"',
+        answers: [
+            { text: 'Tony Cruise', correct: false },
+            { text: 'Keanu Ravens', correct: true },
+            { text: 'Leonardo Decapriyo', correct: false },
+            { text: 'Will Smith', correct: false }
+        ]
+    },
+    {
+        question: 'Which "MCU" Product got a Responsive Box Office Collection',
+        answers: [
+            { text: 'Loki Season 2', correct: true },
+            { text: 'Doctor Strange: the Multiverse of Madness', correct: false },
+            { text: 'Shang Chi', correct: false },
+            { text: 'Black Widow', correct: false }
+        ]
+    }
+];
